496557de085fcd6eccef129dfdd89d56da432cda
liutao <liutaos@qq.com>
2017-10-22 09:37:29 +0800

    添加工具

---
+++ app/src/main/java/com/example/zhijiansha/tools/AbstructProvider.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2017.
+ * liutao
+ * 版权所有
+ */
+package com.example.zhijiansha.tools;
+import java.util.List;
+
+public interface AbstructProvider {
+    public List<?> getList();
+}
