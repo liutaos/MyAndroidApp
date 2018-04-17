2ec87a333d2b02e1f7da35f068353a08acbab670
liutao <liutaos@qq.com>
2018-04-17 20:50:08 +0800

    修改win环境为LinuxUbuntu环境

---
+++ app/src/main/java/com/example/zhijiansha/myapplication/MusicPlayerActivity.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2017.
+ * liutao
+ * 版权所有
+ */
+
+package com.example.zhijiansha.myapplication;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MusicPlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music_playing);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
